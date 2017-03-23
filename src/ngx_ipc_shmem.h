01384e4b01215e5d8cc582ddce5efd399d665222
Nikita Yaschenko <n.yaschenko@corp.vk.com>
2017-03-23 22:18:07 +0300

    Indent shmem. Removed unused functions

--- src/ngx_ipc_shmem.h
+++ src/ngx_ipc_shmem.h
@@ -1,26 +1,30 @@
-#ifndef NCHAN_SHMEM_H
-#define NCHAN_SHMEM_H
-typedef struct {
-  ngx_shm_zone_t        *zone;
-} shmem_t;
+#ifndef NGX_IPC_SHMEM_H
+#define NGX_IPC_SHMEM_H
 
-shmem_t          *shm_create(ngx_str_t *name, ngx_module_t *module, ngx_conf_t *cf, size_t shm_size, ngx_int_t (*init)(ngx_shm_zone_t *, void *), void *privdata);
-ngx_int_t         shm_init(shmem_t *shm);
-ngx_int_t         shm_reinit(shmem_t *shm);
-ngx_int_t         shm_destroy(shmem_t *shm);
-void             *shm_alloc(shmem_t *shm, size_t size, const char *label);
-void             *shm_calloc(shmem_t *shm, size_t size, const char *label);
-void              shm_free(shmem_t *shm, void *p);
+#include <ngx_core.h>
 
-void             *shm_locked_alloc(shmem_t *shm, size_t size, const char *label);
-void             *shm_locked_calloc(shmem_t *shm, size_t size, const char *label);
-void              shm_locked_free(shmem_t *shm, void *p);
 
-void              shmtx_lock(shmem_t *shm);
-void              shmtx_unlock(shmem_t *shm);
+typedef struct ipc_shmem_s     shmem_t;
 
-ngx_str_t        *shm_copy_immutable_string(shmem_t *shm, ngx_str_t *str);
-void              shm_free_immutable_string(shmem_t *shm, ngx_str_t *str);
-void              shm_verify_immutable_string(shmem_t *shm, ngx_str_t *str);
+struct ipc_shmem_s {
+    ngx_shm_zone_t        *zone;
+};
 
-#endif //NCHAN_SHMEM_H
+
+shmem_t    *shm_create(ngx_str_t *name, ngx_module_t *module, ngx_conf_t *cf, size_t shm_size,
+                       ngx_int_t (*init)(ngx_shm_zone_t *, void *), void *privdata);
+ngx_int_t   shm_init(shmem_t *shm);
+ngx_int_t   shm_reinit(shmem_t *shm);
+ngx_int_t   shm_destroy(shmem_t *shm);
+void       *shm_alloc(shmem_t *shm, size_t size, const char *label);
+void       *shm_calloc(shmem_t *shm, size_t size, const char *label);
+void        shm_free(shmem_t *shm, void *p);
+
+void        shmtx_lock(shmem_t *shm);
+void        shmtx_unlock(shmem_t *shm);
+
+ngx_str_t  *shm_copy_immutable_string(shmem_t *shm, ngx_str_t *str);
+void        shm_free_immutable_string(shmem_t *shm, ngx_str_t *str);
+void        shm_verify_immutable_string(shmem_t *shm, ngx_str_t *str);
+
+#endif //NGX_IPC_SHMEM_H
